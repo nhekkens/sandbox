f3f5ae7ec681ac4a583cf3e8a051e9700ff42a07
Nicolay Hekkens <nicolay@wednesdayagency.com>
2014-08-15 10:56:10 +0100

    Delete junk

--- MulberryProto/main.js
+++ MulberryProto/main.js
@@ -1,52 +1,28 @@
 $( document ).ready(function() {
   var currentPos = 0,
-    upTarget = $('#up'),
-    nextPos = 0,
-    downTarget = $('#down'),
-    upTargetSlide = $('#up .slide'),
-    downTargetSlide = $('#down .slide'),
-    upSlideCount = upTargetSlide.length,
-    windowHeight = $(window).height();
-
-  function calcWindowDimensions() {
-    var windowWidth = $(window).width(),
+      upTarget = $('#up'),
+      nextPos = 0,
+      downTarget = $('#down'),
+      upTargetSlide = $('#up .slide'),
+      downTargetSlide = $('#down .slide'),
+      upSlideCount = upTargetSlide.length,
       windowHeight = $(window).height(),
-      halfWindowWidth = windowWidth / 2;
-    setDimensions( halfWindowWidth, windowHeight );
-  };
+      windowWidth = $(window).width(),
+      windowHeight = $(window).height(),
+      halfWindowWidth = windowWidth / 2,
+      dimensions = 'width: ' + halfWindowWidth + 'px; height: ' + windowHeight + 'px;';
 
-  // Set the height and width of the divs
-  function setDimensions( windowWidth, windowHeight ) {
-    var dimensions = 'width: ' + windowWidth + 'px; height: ' + windowHeight + 'px;', // line-height: ' + windowHeight + 'px;'
-      $target = $('.slide');
-    var count = $target.length;
-    // console.log(count);
-    $target.each(function() {
-      $(this).attr( 'style', dimensions );
-    });
-  };
-
-  var upInitPos = 'bottom: ' + windowHeight + 'px;';
-  upTarget.attr( 'style', upInitPos );
-  var downInitPos = 'top: ' + windowHeight + 'px;';
-  downTarget.attr( 'style', downInitPos );
-
-  // Incase of resize, recall
-  $(window).on( 'resize orientationchange', function(event) {
-    console.log(event);
-    calcWindowDimensions();
+  $('.slide').each(function() {
+    $(this).attr( 'style', dimensions );
   });
 
-  // init
-  calcWindowDimensions();
+  upTarget.attr( 'style', 'bottom: ' + windowHeight + 'px;' );
+  downTarget.attr( 'style', 'top: ' + windowHeight + 'px;' );
 
   $('body').swipe( {
-    //Generic swipe handler for all directions
     swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
-      console.log("You swiped " + direction );
       if ( direction === 'up') {
         if ( $('.l'+currentPos).hasClass('active') || $('.r'+currentPos).hasClass('active')) {
-          console.log('valid');
           changePos( 'up' );
         }
         if ( currentPos == 0 ) {
@@ -57,12 +33,10 @@ $( document ).ready(function() {
         changePos( 'down' );
       }
       event.preventDefault;
-    },
-    threshold: 50
+    },threshold: 50
   });
 
   function changePos( increment ) {
-    console.log('Cpos: ' + currentPos + 'nPos: ' + nextPos );
     if ( increment === 'up' ) {
       nextPos = currentPos + 1;
       if (nextPos > (upSlideCount ) ) {
@@ -76,7 +50,6 @@ $( document ).ready(function() {
       }
     }
     currentPos = nextPos;
-    console.log('Cpos after: ' + currentPos);
     if ( currentPos === 0 ) {
       position = windowHeight;
       $('#intro').fadeIn();
@@ -89,20 +62,6 @@ $( document ).ready(function() {
     if ( currentPos === 2 ) {
       position = 0 - windowHeight;
       $('#choose').fadeIn();
-    }
-    if ( currentPos === 3 ) {
-      position = 0 - ( windowHeight * 2 );
-
-      $('#choose').fadeOut();
-    }
-    if ( currentPos === 4 ) {
-      position = 0 - ( windowHeight * 3 );
-    }
-    if ( currentPos === 5 ) {
-      position = 0 - ( windowHeight * 4 );
-    }
-    if ( currentPos === 6 ) {
-      position = 0 - ( windowHeight * 5 );
     }
     var upPosition = 'bottom: ' + position + 'px;',
         downPosition = 'top: ' + position + 'px;';
@@ -120,7 +79,6 @@ $( document ).ready(function() {
       }
     }
   });
-
   $('.r1').swipe( {
     tap:function(event, target) {
       if ( $('.l1').hasClass('active') ) {
@@ -131,7 +89,6 @@ $( document ).ready(function() {
       }
     }
   });
-
   $('.l2').swipe( {
     tap:function(event, target) {
       if ( $('.r2').hasClass('active') ) {
@@ -142,7 +99,6 @@ $( document ).ready(function() {
       }
     }
   });
-
   $('.r2').swipe( {
     tap:function(event, target) {
       if ( $('.l2').hasClass('active') ) {
@@ -154,14 +110,8 @@ $( document ).ready(function() {
     }
   });
 
-  // START IMAGES
-  var images = [
-    'one.jpg',
-    'two.jpg',
-    'three.jpg'
-  ];
-
-  var index = 0;
+  var images = [ 'one.jpg', 'two.jpg', 'three.jpg' ],
+      index = 0;
   setInterval(change_up, 3000);
   function change_up(){
     index = (index + 1 < images.length) ? index + 1 : 0;
